59d841324a6aa20e82de36115da89a95e27ce8e8
Llugani <93303476+Llugani@users.noreply.github.com>
2022-03-31 20:43:54 -0400

    Update ADDER.v

--- ADDER.v
+++ ADDER.v
@@ -1,22 +1,29 @@
 
-module ADDER (clk, rst, PC_in_4, PC_out_4);
+module PC(clk, rst, PC_in, PC_out);
 
 input clk, rst;
-input [3:0] PC_in_4;
-output reg [7:0] PC_out_4;
+input [7:0] PC_in;
+output reg [7:0] PC_out;
+
+initial begin
+PC_out = 8'b0;
+end
 
 
-always @ (posedge clk) begin
+always @ (posedge clk or posedge rst) begin
 
 if (rst) begin
-
-PC_out_4 <= 8'b0;
+PC_out = 8'b0;
 end
 
-else begin 
-PC_out_4 <= PC_in_4+8'd4;
-end
+else begin
+
+PC_out = PC_in;
+
 
 end
+ 
+end
+
 
 endmodule
